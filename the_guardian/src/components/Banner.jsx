fdda8af9aa7eafff456f4633d9ecdb8074f1e939
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-03 20:11:43 -0400

    magnify glass img

--- the_guardian/src/components/Banner.jsx
+++ the_guardian/src/components/Banner.jsx
@@ -16,9 +16,11 @@ class Banner extends Component {
                 <div>
                     <ul className="top-nav">
                         <li> Search jobs </li>
+                        <img className="login-picture" src="https://cdn3.iconfinder.com/data/icons/login-4/512/LOGIN-10-512.png" alt="" width="25px" height="25px" />
                         <li> Sign in </li>
+                        <img className="magnify-glass" src="https://image.flaticon.com/icons/png/512/49/49116.png" alt="" width="25px" height="25px"/>
                         <li> Search </li>
-                        <img src="https://breedlondon.com/wp-content/uploads/2018/12/the-guardian.png" width="280px" />
+                        <img src="https://breedlondon.com/wp-content/uploads/2018/12/the-guardian.png" width="280px" alt="" />
                         <li> US edition</li>
                     </ul>
                 </div>
