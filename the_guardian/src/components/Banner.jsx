81e8af48c09d697b0c7bc6defc4b34380f76edd9
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-03 22:14:42 -0400

    bottom nav bar has whitespace

--- the_guardian/src/components/Banner.jsx
+++ the_guardian/src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Banner extends Component {
     state = {}
     render() {
-        return (<div>
+        return (<div className="banner-container">
             <div className="banner">
                 <div className="guardian-box">
                     <h1 className="guardian__heading--yellow"> Support The Guardian </h1>
@@ -18,12 +18,22 @@ class Banner extends Component {
                         <li> Search jobs </li>
                         <img className="login-picture" src="https://cdn3.iconfinder.com/data/icons/login-4/512/LOGIN-10-512.png" alt="" width="25px" height="25px" />
                         <li> Sign in </li>
-                        <img className="magnify-glass" src="https://image.flaticon.com/icons/png/512/49/49116.png" alt="" width="25px" height="25px"/>
+                        <img className="magnify-glass" src="https://image.flaticon.com/icons/png/512/49/49116.png" alt="" width="25px" height="25px" />
                         <li> Search </li>
                         <img src="https://breedlondon.com/wp-content/uploads/2018/12/the-guardian.png" width="280px" alt="" />
                         <li> US edition</li>
                     </ul>
                 </div>
+            </div>
+            <div>
+                <ul className="bottom-navbar">
+                    <li className="bottom-list">News</li>
+                    <li className="bottom-list">Opinion</li>
+                    <li className="bottom-list">Sport</li>
+                    <li className="bottom-list">Culture</li>
+                    <li className="bottom-list">Lifestyle</li>
+                    <li className="bottom-list">More</li>
+                </ul>
             </div>
         </div>);
     }
