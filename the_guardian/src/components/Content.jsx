1d4abe8267aa7c65f22814aada91aa2faf5dcb45
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-05 21:26:20 -0400

    adding article text to the components

--- the_guardian/src/components/Content.jsx
+++ the_guardian/src/components/Content.jsx
@@ -35,9 +35,24 @@ class Content extends Component {
                 <div>
                     <Left />
                 </div>
-                <div>
-                    <h4>Trump impeachment / Schumer says Senate trail without witnesses would be 'cover up'</h4>
-                    <img className="charles-schumer" src={ Chumer } alt="whyy"></img>
+                <div className="chumer-text-image">
+                    <div>
+                        <div>
+                            <h4 className="chumer-text">Trump impeachment /</h4>
+                            <h4 className="chumer-text">Schumer says</h4>
+                            <h4 className="chumer-text">Senate trail without</h4>
+                            <h4 className="chumer-text">witnesses would be</h4>
+                            <h4 className="chumer-text">'cover up'</h4>
+                        </div>
+                        <div>
+                            <h5 className="smallblock-text"> Senate minority leader criticized</h5>
+                            <h5 className="smallblock-text"> the White House for its handling</h5>
+                            <h5 className="smallblock-text"> of the impeachmnt inquiry.</h5>
+                            <h5 className="smallblock-text"> saying 'trails have witnesses'</h5>
+                        </div>
+
+                    </div>
+                    <img className="charles-schumer" src={Chumer} alt="whyy" width="450"></img>
                 </div>
                 {/*                 <ul>
                     {this.state.data.map((recipe, id) => <li key={id}> {recipe.title} </li>)}
