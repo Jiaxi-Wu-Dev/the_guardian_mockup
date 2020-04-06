2d30a5fe5afcdd1caf8b4c1ba9b86973bfc55690
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-05 21:45:31 -0400

    pictures not appearing where they should

---
+++ the_guardian/src/components/Right.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Right extends Component {
+    state = {}
+    render() {
+        return (<div>
+            <img className="harvey" src={Harvey} alt="" />
+        </div>);
+    }
+}
+
+export default Right;
